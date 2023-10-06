8ac117cb7481ecf1faa4d8ee8fb182d4aa215875
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-10-06 16:22:25 -0300

    angular

--- AngularProjects/meu-primeiro-angular/src/app/app-routing.module.ts
+++ AngularProjects/meu-primeiro-angular/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'header', component: HeaderComponent },
+{ path: 'footer', component: FooterComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- AngularProjects/meu-primeiro-angular/src/app/app.module.ts
+++ AngularProjects/meu-primeiro-angular/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ContatoComponent } from './contato/contato.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    SobreComponent,
+    ContatoComponent
   ],
   imports: [
     BrowserModule,
