670f2a680e659966e2fd0ec6b13560725814b6b9
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-10-10 16:55:31 -0300

    angular

--- AngularProjects/meu-primeiro-angular/src/app/app.module.ts
+++ AngularProjects/meu-primeiro-angular/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,7 +23,8 @@ import { FormularioComponent } from './formulario/formulario.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
