bc6aa7d888e7029d467e44a430a364c35c0dd476
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-10-10 14:41:36 -0300

    angular

--- AngularProjects/meu-primeiro-angular/src/app/app-routing.module.ts
+++ AngularProjects/meu-primeiro-angular/src/app/app-routing.module.ts
@@ -6,12 +6,16 @@ import { HomeComponent } from './home/home.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { ContatoComponent } from './contato/contato.component';
 
+import { FormularioComponent } from './formulario/formulario.component';
+
+
 const routes: Routes = [
-{ path: 'header', component: HeaderComponent },
-{ path: 'footer', component: FooterComponent },
-{ path: '', component: HomeComponent },
+{ path: 'header', component: HeaderComponent }, //não precisa criar
+{ path: 'footer', component: FooterComponent }, //não precisa criar
+{ path: '', component: HomeComponent }, //está vazia para não precisar digitar /home e já entrar nessa página quando abrir o site
 { path: 'sobre', component: SobreComponent },
 { path: 'contato', component: ContatoComponent },
+{ path: 'formulario', component: FormularioComponent },
 ];
 
 @NgModule({
---
+++ AngularProjects/meu-primeiro-angular/src/app/usuario.model.ts
@@ -0,0 +1,6 @@
+// src/app/usuario.model.ts
+export interface Usuario {
+    nome: string;
+    email: string;
+    // Adicione outros campos, se necessário
+    }
