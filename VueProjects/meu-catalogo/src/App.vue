e93aded7f2188955744e398e517aedafca4443e0
rafaelmoura23 <rafaelzinmoura23@gmail.com>
2023-10-20 13:47:45 -0300

    vue_catalogo_filmes

--- VueProjects/meu-catalogo/src/App.vue
+++ VueProjects/meu-catalogo/src/App.vue
@@ -1,19 +1,24 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <header>
+      <h1>Meu Catálogo de Filmes</h1>
+    </header>
+    <main>
+      <CatalogoDeFilmes />
+    </main>
+  </div>
 </template>
-
+  
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CatalogoDeFilmes from "@/components/CatalogoDeFilmes.vue";
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    CatalogoDeFilmes,
+  },
+};
 </script>
-
+  
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -21,6 +26,13 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 20px;
+}
+
+header {
+
+  background-color: #35495e;
+  color: #fff;
+  padding: 10px 0;
 }
 </style>
